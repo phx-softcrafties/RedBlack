46862f0c918f19ee88a18f0c1c12fbc901d30d17
John Shumway <john.shumwayjr@gmail.com>
2012-08-15 22:48:18 -0700

    A little more...

--- ruby/test/tree_test.rb
+++ ruby/test/tree_test.rb
@@ -4,26 +4,32 @@ require 'app/node'
 
 class TestTree < Test::Unit::TestCase
     def create_tree
-        node = Node.new 3
-        tree = Tree.new node
+        root = Node.new 3
+        @tree = Tree.new root
     end
 
-    def setup
-        @tree = create_tree
+    def create_tree_with_one_node
+        root = Node.new 3
+        root.left = Node.new 1
+        @tree = Tree.new root
     end
+
 
     def test_create
+        create_tree
         expect = Node.new 3
         assert_equal expect, @tree.root
     end 
 
     def test_add_second_node_with_lesser_value
+        create_tree
         @tree.add Node.new 1
         expect = Node.new 1
         assert_equal expect.to_s, @tree.root.left.to_s
     end
 
     def test_add_second_node_with_greater_value
+        create_tree
         @tree.add Node.new 5
         expect = Node.new 5
         assert_equal expect.to_s, @tree.root.right.to_s
